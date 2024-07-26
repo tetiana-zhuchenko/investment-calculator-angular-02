f284e2859504869ee021207b2d028f6f6e1f7aa8
Tania Zhuchenko <taniyhamail@gmail.com>
2024-07-26 20:36:27 +0300

    feat: added two-way-binding form

---
+++ src/app/investments-results/investments-results.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investments-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './investments-results.component.html',
+  styleUrl: './investments-results.component.css'
+})
+export class InvestmentsResultsComponent {
+
+}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '10';
+
+  onSubmit() {
+    console.log({
+      initialInvestment: this.initialInvestment,
+      annualInvestment: this.annualInvestment,
+      expectedReturn: this.expectedReturn,
+      duration: this.duration,
+    });
+  }
+}
