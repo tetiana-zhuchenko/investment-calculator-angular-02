d0802ab5ca6001f9ba998c599f872fd9670e1fca
Tania Zhuchenko <taniyhamail@gmail.com>
2024-07-29 16:09:48 +0300

    feat: added cross-components comunication

--- src/app/investments-results/investments-results.component.ts
+++ src/app/investments-results/investments-results.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { InvestmentResult } from './investments-results.model';
 
 @Component({
   selector: 'app-investments-results',
   standalone: true,
   imports: [],
   templateUrl: './investments-results.component.html',
-  styleUrl: './investments-results.component.css'
+  styleUrl: './investments-results.component.css',
 })
 export class InvestmentsResultsComponent {
-
+  @Input() results?: InvestmentResult[];
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -9,17 +10,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  initialInvestment = '0';
-  annualInvestment = '0';
-  expectedReturn = '5';
-  duration = '10';
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  expectedDuration = '10';
 
   onSubmit() {
-    console.log({
-      initialInvestment: this.initialInvestment,
-      annualInvestment: this.annualInvestment,
-      expectedReturn: this.expectedReturn,
-      duration: this.duration,
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.expectedDuration,
     });
   }
 }
